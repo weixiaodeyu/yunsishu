c13568c66342cb25975862260c1bee2fabe4ef41
weixiaodeyu <953512098@qq.com>
2018-09-26 17:03:34 +0800

    云私塾最新
    重新生成

--- files/云私塾业务模式/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o],p,_(q,r,s,t,u,v,w,_(),x,_(y,z,A,B,C,_(D,E,F,G),H,null,I,B,J,B,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
-var b="url",c="云私塾业务模式.html",d="generationDate",e=new Date(1537861315988.98),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="tongzhaoshuxing",o="NewVariable",p="page",q="packageId",r="d9ce0bbab66e4ad7a405b5c3831992f6",s="type",t="Axure:Page",u="name",v="云私塾业务模式",w="notes",x="style",y="baseStyle",z="627587b6038d43cca051c114ac41ad32",A="pageAlignment",B="near",C="fill",D="fillType",E="solid",F="color",G=0xFFFFFFFF,H="image",I="imageHorizontalAlignment",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
-return _creator();
-})());
